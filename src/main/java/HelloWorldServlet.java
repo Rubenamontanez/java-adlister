ccd875bc5b8a266252eac71701507bb1ae3f9361
Ruben Montanez <ruben.a.montanez@gmail.com>
2020-02-25 20:16:41 -0600

    hope

---
+++ src/main/java/HelloWorldServlet.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String name = request.getParameter("name");
+        if (name.equals("")){
+            out.println("<h1>Hello World!</h1>");
+        }
+        else {
+            out.println("<h1>Hello, " + name + "!</h1>");
+        }
+    }
+}
